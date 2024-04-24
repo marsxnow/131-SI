594ed2d6d8a2f6ecfcb625cf3fbf372f7e719992
marsxnow <raxelportiz@gmail.com>
2024-04-24 15:11:00 -0700

    Addind the vector files

---
+++ vector/vector.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<memory>
+using namespace std;
+class Array{
+// initialize private variables
+private:
+
+// initialize public variables
+public:
+
+// initialize the array
+Array(int capacity):capacity(capacity), size(0){
+  arr = make_unique<int[]>(capacity);
+}
+// create a insert function
+
+// create a delete function
+
+// create a get function
+
+// create a get size function
+
+// create a pop function
+
+// create a traverse function
+
+};
